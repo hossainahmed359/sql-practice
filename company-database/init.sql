0c30b773be0d93ca271fc22598c8416476c72c25
hossainahmed359 <hossainahmed359@gmail.com>
2023-07-21 21:19:38 +0600

    Add all tables

--- company-database/init.sql
+++ company-database/init.sql
@@ -48,3 +48,22 @@ CREATE TABLE client(
   REFERENCES branch(branch_id)
   ON DELETE SET NULL
 );
+
+-- works_with table
+CREATE TABLE works_with(
+  emp_id INT,
+  client_id INT,
+  total_sales INT,
+  PRIMARY KEY(emp_id, client_id),
+  FOREIGN KEY(emp_id) REFERENCES employee(emp_id) ON DELETE CASCADE,
+  FOREIGN KEY(client_id)REFERENCES client(client_id) ON DELETE CASCADE
+);
+
+-- branch_supplier table
+CREATE TABLE branch_supplier(
+  branch_id INT,
+  supplier_name VARCHAR(40),
+  supplier_type VARCHAR(40),
+  PRIMARY KEY(branch_id, supplier_name),
+  FOREIGN KEY(branch_id) REFERENCES branch(branch_id) ON DELETE CASCADE
+);
