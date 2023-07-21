caa46448fd9460d27583bd72c34d90acc5131703
hossainahmed359 <hossainahmed359@gmail.com>
2023-07-21 20:55:15 +0600

    employee branch and client table

--- company-database/init.sql
+++ company-database/init.sql
@@ -8,6 +8,43 @@ CREATE TABLE employee(
   birth_date DATE,
   sex VARCHAR(1),
   salary INT,
-  super_id INT, -- foreign key
+  super_id INT, -- foreign key 
   branch_id INT -- foreign key
+);
+
+-- NOTE: FOREIGN KEYs can not be initialized before perticular table creation
+
+-- branch table
+CREATE TABLE branch(
+  branch_id INT PRIMARY KEY,
+  branch_name VARCHAR(40),
+  mgr_id INT,
+  mgr_start_date DATE,
+  FOREIGN KEY(mgr_id) 
+  REFERENCES employee(emp_id) 
+  ON DELETE SET NULL
+);
+
+-- declare foreign keys
+
+-- super_id / superviser
+ALTER TABLE employee
+ADD FOREIGN KEY(super_id) 
+REFERENCES employee(emp_id)
+ON DELETE SET NULL;
+
+-- branch_id
+ALTER TABLE employee
+ADD FOREIGN KEY(branch_id) 
+REFERENCES branch(branch_id)
+ON DELETE SET NULL;
+
+-- client table
+CREATE TABLE client(
+  client_id INT PRIMARY KEY,
+  client_name VARCHAR(40),
+  branch_id INT,
+  FOREIGN KEY(branch_id)
+  REFERENCES branch(branch_id)
+  ON DELETE SET NULL
 );
